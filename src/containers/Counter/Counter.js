c21cf0aa08e42988a497e6333a821bc674fa9878
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-19 14:59:44 +0100

    deleting immutable

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -34,6 +34,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubstractCounter} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -41,7 +48,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
@@ -61,6 +69,14 @@ const mapStateToProps = state => {
             type:'SUBSTRACT',
             value: 5
         }),
+        onStoreResult: () => dispatch({
+            type:'STORE_RESULT'
+        }),
+        onDeleteResult: (id) => dispatch({
+            type:'DELETE_RESULT',
+            resultElId: id
+        }),
+
 
     };
  };
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,6 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
@@ -7,19 +8,38 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
+                ...state,
                 counter: state.counter + 1
             }
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.value
             }
         case 'SUBSTRACT':
             return {
+                ...state,
                 counter: state.counter - action.value
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+            }
+        case 'DELETE_RESULT':
+            // const id = 2;
+            // const newArr = [...this.state.results];
+            // newArr.splice(id,1);
+            
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            return{
+                ...state,
+                results: updatedArray
             }
     }
 
