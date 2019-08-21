dda3b80e1149252ca6e08bd0c00a278a027ee3d8
Florian Celestin <F.Celestin@epyx.co.uk>
2019-08-21 10:16:32 +0100

    updateObject utility function

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
-
+import { updateObject } from '../utility';
 const initialState = {
     results: []
 }
@@ -9,20 +9,16 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             // make changes to the state here not in the action creator
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result})
-            }
+
+            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) })
+
         case actionTypes.DELETE_RESULT:
             // const id = 2;
             // const newArr = [...this.state.results];
             // newArr.splice(id,1);
-            
+
             const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return{
-                ...state,
-                results: updatedArray
-            }
+            return updateObject(state, { results: updatedArray })
     }
 
     return state;
